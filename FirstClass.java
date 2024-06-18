211e8a18db0fc82d53c8a4e41442d153c0555481
Thejas <thejasganiga150503@gmail.com>
2024-06-18 18:57:28 +0530

    Add files via upload
    Basics looked fine , hoping the further lectures to go well !!

---
+++ FirstClass.java
@@ -0,0 +1,50 @@
+import java.util.* ;
+public class FirstClass {
+    public static void main (String args []){
+        // System.out.println("Hello World in java language");
+        // System.out.print("Hello World in java language\n");
+        // System.out.print("Hello World");
+
+        // the use of \n
+
+        // System.out.println("Hello World\nHello World From Thejas\nHello World in java Language");
+
+        // System.out.println("*");
+        // System.out.println("* *");
+        // System.out.println("* * *");
+        // System.out.println("* * * *");
+
+        // System.out.println("*\n* *\n* * *\n* * * *\n");
+
+        // int a = 10 ;
+        // int b = 20 ;
+        // String name = "Thejas" ;
+        // int age = 21 ;
+        // double price = 25.56 ;
+        // name = "Rahul" ;
+
+        // int a = 10 ; 
+        // int b = 20 ;
+        // int sum = a + b ;
+        // System.out.println(sum);
+        // int sub = b - a ;
+        // System.out.println(sub);
+        // int mul = a * b ;
+        // System.out.println(mul);
+        // int div = a / b ;
+        // System.out.println(div);
+
+        // Scanner sc = new Scanner (System.in) ; 
+        // System.out.println("Enter The name ");
+        // String name = sc.nextLine() ; 
+        // System.out.println("The Name is "+name);
+        // nextLine() , nextInt() , nextFloat() , nextLong() and next()
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the value of the variables :");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b ;
+        System.err.println(sum);
+    } 
+}
