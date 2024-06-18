c36eda279fc65e61a574d9d9811d730e9b586ab2
Thejas <thejasganiga150503@gmail.com>
2024-06-18 19:14:11 +0530

    Add files via upload
    And The Other One ! , from basics lol :)

---
+++ FirstAssignment.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class FirstAssignment {
+    public static void main (String args []){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter The Radius Of The Circle :");
+        // int radius = sc.nextInt();
+        // float area = 3.142f * radius * radius ;
+        // System.out.println("Area is : "+area);
+        // float circumference = 2 * 3.142f * radius ; 
+        // System.out.println("Circumference is "+circumference);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int n = sc.nextInt();
+        System.out.println("The Table goes from :");
+        System.out.println(n+" X 1 =  "+n*1);
+        System.out.println(n+" X 2 =  "+n*2);
+        System.out.println(n+" X 3 =  "+n*3);
+        System.out.println(n+" X 4 =  "+n*4);
+        System.out.println(n+" X 5 =  "+n*5);
+        System.out.println(n+" X 6 =  "+n*6);
+        System.out.println(n+" X 7 =  "+n*7);
+        System.out.println(n+" X 8 =  "+n*8);
+        System.out.println(n+" X 9 =  "+n*9);
+        System.out.println(n+" X 10 = "+n*10);
+
+        // The above code is written without using the for loop since it is basic !!!
+    }
+}
