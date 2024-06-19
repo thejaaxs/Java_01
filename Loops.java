ba38750b1a72111dbcbcbcf15e7d0c691866da82
Thejas <thejasganiga150503@gmail.com>
2024-06-20 00:47:07 +0530

    Add files via upload
    Done with Loops SomeHow , ThankYou Apna College !!

---
+++ Loops.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Loops {
+    public static void main (String args []){
+        // for(int i = 1 ; i<=100 ; i++){
+        //     System.out.println("Hello World");
+        // }
+
+            // for(int i = 0 ; i<= 10 ; i++){
+            //     System.out.print(i+" ");
+            // }
+            // same code with the help of the while loop
+
+                // int i = 0 ;
+                // while(i<=10){
+                //     System.out.print(i+" ");
+                //     i++;
+            // }
+            // same code with the help of the Do-While loop 
+
+                    // int i = 0 ;
+                    // do {
+                    //     System.out.print(i+"\t");
+                    //     i++;
+                    // }while(i<=10);
+
+    //                         Scanner sc = new Scanner (System.in) ;
+    //                         System.out.println("Enter the number :");
+    //                         int n = sc.nextInt();
+    //                         int sum = 0 ;
+    //                         for(int i = 0 ; i <= n ; i++){
+    //                             sum = sum + i ;
+                               
+    //                         }
+    //                         System.out.println(sum);
+
+                                        // Scanner sc = new Scanner(System.in);
+                                        // System.out.println("Enter the number of the table :");
+                                        // int n = sc.nextInt() ; 
+                                        // for(int i = 1 ; i<=10 ; i++){
+                                        //     System.out.println(n+" X "+i+" = "+n*i);
+                                        // }
+    }
+}
