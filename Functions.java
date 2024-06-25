e8e4fa1c22f4b40a9f6a9a37fca9d05dad4c0d17
Thejas <thejasganiga150503@gmail.com>
2024-06-25 19:38:33 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class Functions {
+//     public static void printMyName (String name){
+//         System.out.println("The Name is "+name);
+//         return ; 
+//     }
+//     public static void main (String args []){
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter The Name :");
+// String name = sc.nextLine();
+// printMyName(name);
+//     }
+
+// public static int returnSum (int num1 , int num2){
+//     int sum = num1 + num2 ;
+//     return sum ;
+// }
+// public static void main (String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int num1 = sc.nextInt();
+//     int num2 = sc.nextInt();
+//     int sum = returnSum(num1, num2) ; 
+//     System.out.println("The sum is "+sum);
+// }
+
+// public static Double calculateProduct(Double a , double b){
+//      return a * b ;
+// }
+// public static void main (String args []){
+//     Scanner sc = new Scanner (System.in);
+//     double a = sc.nextDouble();
+//     double b = sc.nextDouble();
+//     System.out.println("The product is "+ calculateProduct( a, b));
+
+// }
+
+// public static int calculateFactorial(int n){
+//     int factorial =1 ;
+
+    
+//     if(n<0){
+//         System.out.println("Error");
+//     }
+  
+// for(int i = n ; i>=1 ; i--){
+//     factorial = factorial * i ;
+// }
+// // System.out.println(factorial);
+
+// return factorial; 
+// }
+//  public static void main(String args []){
+// Scanner sc = new Scanner (System.in);
+// int n = sc.nextInt();
+// System.out.println("The factorial is "+calculateFactorial(n));
+//  }
+
+}
